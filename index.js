1cc8538b091678d4ecd845f1d9459e87d470723f
Yell Min Thu <120482584+JunHaoyell@users.noreply.github.com>
2026-02-04 19:50:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+import TelegramBot from "node-telegram-bot-api";
+import express from "express";
+
+const token = process.env.BOT_TOKEN;
+const WEBAPP_URL = process.env.WEBAPP_URL;
+
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🧧 点开领取新年红包", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "🎉 打开红包",
+            web_app: { url: WEBAPP_URL }
+          }
+        ]
+      ]
+    }
+  });
+});
+
+app.get("/", (_, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(process.env.PORT || 3000);
 
